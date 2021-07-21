ccb18c5bd6a5e70e9aa9da5178ade406c60af11a
Apurba Debnath <apurba871@gmail.com>
2021-07-21 20:37:09 +0530

    Working with States and React Hooks

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,16 +2,22 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDesc from "./ExpenseDesc";
 import Card from "./Card";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <ExpenseDesc desc={props.desc} />
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
